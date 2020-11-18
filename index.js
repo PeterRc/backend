e3df9da7360a765d6e9bbd26821acc4397e2b0a5
Pedro Rc <peter.rc29@gmail.com>
2020-11-17 20:49:13 -0600

    anexo del apartado rutas como respuesta del servidor

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const express = require('express');
 // Crear servidor express
 const app = express();
 
+// Rutas 
+app.get('/', (req, res) => {
+    console.log('Se recibió una petición en /');
+    res.json({
+        ok: true,
+        msg: "Recibido",
+    });
+}
+);
+
 // Configuracion del puerto 
 app.listen(4000, () => {
     console.log(`Backend corriendo en el puerto ${4000}`);    
