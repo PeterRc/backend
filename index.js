b5a5ecc8c0dd0bdecc4ed86c03d139c5bc6b273b
Pedro Rc <peter.rc29@gmail.com>
2020-11-17 20:31:46 -0600

    creacion del servidor express

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hola mundo!");
+const express = require('express');
+
+// Crear servidor express
+const app = express();
+
+// Configuracion del puerto 
+app.listen(4000, () => {
+    console.log(`Backend corriendo en el puerto ${4000}`);    
+})
